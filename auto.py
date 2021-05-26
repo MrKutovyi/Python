389fd1eb7bfd19928f3f3a27040b1433631dd4d8
Дмитрий Кутовой <dkutovoy@web100-ck.local>
2021-05-26 17:17:29 +0300

    Initial commit

---
+++ auto.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('C:\Python\chromedriver')
+
+driver.get('https://www.gorgany.com/')
+driver.maximize_window()
+time.sleep(2)
+
+login_button = driver.find_element_by_css_selector("button[class='user hidden-xs hidden-sm hidden-md login']")
+login_button.click()
+time.sleep(3)
+
+email_field = driver.find_element_by_css_selector("input[placeholder='Ваша e-mail адреса']")
+driver.execute_script("arguments[0].click();", email_field)
+email_field.send_keys("mr.kutovyi@gmail.com")
+
+pass_field = driver.find_element_by_css_selector("input[placeholder='Ваш пароль']")
+driver.execute_script("arguments[0].click();", pass_field)
+pass_field.send_keys("ab63cbee6a")
+time.sleep(5)
+
+login = driver.find_element_by_css_selector("button[id='button-login']")
+login.click()
+
+
+
+# driver.quit()
